553cd5e2dd7c202a7f40a21ae8e53ef9a38a84f5
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-16 02:23:44 +0200

    add ValidateSchema's middleware

---
+++ middlewares/validateSchema.js
@@ -0,0 +1,22 @@
+const { ErrorHandler } = require("./error");
+const validateSchema = (schema, property, abortEarly = false) => {
+    return (req, res, next) => {
+        if (!req[property]) {
+            req[property] = {}; // for the case if called with empty body, to return the errors.
+        }
+        const { error } = schema.validate(req[property], {
+            abortEarly,
+        });
+        const valid = error == null;
+        if (valid) {
+            next();
+        } else {
+            const { details } = error;
+            const errors = details.map((detail) => detail.message);
+            const err = new ErrorHandler(400, errors);
+            next(err);
+        }
+    };
+};
+
+module.exports = validateSchema;
