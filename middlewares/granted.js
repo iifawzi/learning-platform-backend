e6fa5f53581b9aa9da1b5fadf78bb011151af6b2
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-21 17:38:41 +0200

    add Role based access control

---
+++ middlewares/granted.js
@@ -0,0 +1,19 @@
+const roles = require("../startup/accessControl");
+const { ErrorHandler } = require("../utils/error");
+
+module.exports = (action,resource)=>{
+    return (req,res,next)=>{
+        try {
+            let role = req.requseter.role;
+            const isGranted = roles.can(role)[action](resource).granted;
+            if (isGranted){
+                next();
+            }else {
+                throw new ErrorHandler(403,"Not Authorized");
+            }
+        }catch(err){
+            next(err);
+        };
+    }
+
+}
